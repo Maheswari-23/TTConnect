4c8603608dbf3c6f22d172e2d7b1bfb7d6873ea6
Maheswari-23 <140819808+Maheswari-23@users.noreply.github.com>
2026-02-18 13:53:34 +0530

    Update API base URL for axios instance

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ttconnect.onrender.com/",
+  baseURL: "https://ttconnect-backend.onrender.com",
 });
 
 export default API;
